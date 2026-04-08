14c264f5149107b5bac52402a5cfcff7f1850d6c
Chaykin <chay@Mac-002.lan>
2026-04-08 12:55:07 +0200

    tune(gauge): lighten background icon when show_value is enabled

--- seagull-room-card.js
+++ seagull-room-card.js
@@ -729,7 +729,7 @@ class SeagullRoomCard extends HTMLElement {
             </span>`)
         : (gaugeEnabled && gaugeShowValue)
           ? `<span style="position:relative;z-index:1;width:100%;height:100%;display:block;font-family:${this._esc(String(numberFontFamily))};">
-              <ha-icon icon="${this._esc(icon)}" style="position:absolute;left:50%;top:50%;transform:translate(-50%,-50%);color:${this._esc(iColor)};opacity:.55;--mdc-icon-size:${Math.max(12, Math.round(btnSize * 0.5))}px;"></ha-icon>
+              <ha-icon icon="${this._esc(icon)}" style="position:absolute;left:50%;top:50%;transform:translate(-50%,-50%);color:${this._esc(this._toRgba(iColor, 0.35))};opacity:1;--mdc-icon-size:${Math.max(12, Math.round(btnSize * 0.5))}px;"></ha-icon>
               <span style="position:absolute;left:50%;top:50%;transform:translate(-50%,-50%);display:inline-flex;align-items:flex-start;line-height:1;color:${this._esc(iColor)};font-size:${valueFontPxBig}px;font-weight:${this._esc(String(numberFontWeight))};">
                 <span>${this._esc(gaugeValue)}</span>
                 ${climatSuffix ? `<span style="margin-left:0px;margin-top:0.12em;font-size:${unitFontPxBig}px;opacity:.95;line-height:1;">${this._esc(climatSuffix)}</span>` : ""}
